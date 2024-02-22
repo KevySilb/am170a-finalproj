18671065707ce04071368e9d15660522ee66d580
Kevin Silberberg <kevinsilberberg@pop-os.localdomain>
2024-02-21 16:07:39 -0800

    modified data.py to simulate the dynamics of insulin extraction under the logistic model

--- data.py
+++ data.py
@@ -1,28 +1,46 @@
 import pandas as pd
+import numpy as np
 from matplotlib import pyplot as plt
 import scipy.integrate as itg
 '''1mg/hour per mL for a bacterial solution containing 0.1g of dry bacteria per mL'''
 
 class simulateBacteria:
     """
-    alpha : bacteria growth rate
-    beta  : insulin potency
-    kappa : carrying capacity 
+    bacteria growth rate = 0.0355
+    insulin potency = 1.68
+    insulin production rate 5.41e-08 mg insulin / (per million bacteria * min)
+    self.k = carrying capacity F1 (100), F2 (1,000), F3 (10,000)
+    self.a = rate of insulin extraction
 
     self.sol.y contants (bacteria(t), insulin(t))
     """
-    def __init__(self, kappa, init, t_points) -> None:
-        self.a, self.b, self.g = 0.0355, 1.68, 5.41e-08
-        self.k = kappa
+    def __init__(self, params, init, t_points) -> None:
+        self.k, self.a = params
         self.x0, self.y0 = init
         self.t_points = t_points
+        # cumulative sum of insulin extracted 
+        self.Insulin = 0
+        # a history of extracted insulin per time step 
+        self.cumsumInsulin = []
+        # the solution of the ivp
         self.sol = self.simulate()
     
     def func(self, t, r):
-        x, y = r
-        dx_dt = self.a * x * (1-(x/self.k)) - self.b * x * y 
-        dy_dt = self.g * x
-        return dx_dt, dy_dt
+        """
+        x1 represents millions of bacteria in solution at a particular point in time 
+        x2 represents mg of insulin per mL
+        """
+        x1, x2 = r
+        # insulin extraction is proportional to the amount of insulin in solution (no insulin = no extraction)
+        extractedInsulin = self.a * x2
+        # running total of extracted insulin
+        self.Insulin += extractedInsulin
+        # append new total to list for plotting 
+        self.cumsumInsulin.append(self.Insulin)
+        # the ivp:
+        dx1_dt = 0.0355 * x1 * (1-(x1/self.k)) - 1.68 * x1 * x2 
+        dx2_dt = 5.41e-08 * x1 - extractedInsulin
+        return dx1_dt, dx2_dt
     
     def simulate(self):
         # time interval
@@ -36,8 +54,24 @@ class simulateBacteria:
         return result
 
 def main():
-
+    # get data
+    data = pd.read_csv('data_with_insulin.txt')
+    # initial conditions (100 million bacteria, 0 insulin)
     init = (100, 0)
+    # define parameters (1000x sugar concentration, rate of insulin extraction))
+    params = (10000, 1.2e-3)
+
+    sim = simulateBacteria(params, init, data['t'].values)
+    bac, insul = sim.sol.y
+    
+    plt.plot(data['t'].values, bac, label = 'bacteria')
+    plt.plot(data['t'].values, insul, label = 'insulin')
+    plt.plot(np.linspace(0, 600, len(sim.cumsumInsulin)), sim.cumsumInsulin, label = 'insulin extracted')
+    plt.yscale('log')
+    plt.legend()
+
+    plt.show()
+
     
 
 
